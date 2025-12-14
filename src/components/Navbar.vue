6db90e2356a5b0364d1e1df964af48503ae1eda0
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2025-12-14 19:49:26 +0530

    gitflow-feature-stash: navbar-design

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="flex justify-between items-center p-6 bg-opacity-50 relative z-20">
+        <div class="text-3xl font-bold bg-liner-to-r from-blue-400 to-cyan-400">
+
+        </div>
+    </header>
+   
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
